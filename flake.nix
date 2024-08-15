fb8f3f27e14c6d05cea4f5f312a57f04116d91a1
harryprayiv <harryprayiv@gmail.com>
2024-08-15 17:11:36 -0400

    chez scheme back end installation step 1

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "Stock View";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     purescm.url = "github:/purescm/purescm";
@@ -11,32 +12,36 @@
     flake-compat.flake = false;
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    purescript-overlay,
-    purescm,
-    ...
-  }: let
+  outputs = { self, nixpkgs, purescm, purescript-overlay, ... }: let
     name = "stockView";
 
     supportedSystems = [
       "aarch64-darwin"
       "x86_64-darwin"
-      "x86_64-darwin" 
       "x86_64-linux"
     ];
 
     forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
   in {
     devShell = forAllSystems (system: let
-      overlays = [purescript-overlay.overlays.default];
-      pkgs = import nixpkgs {inherit system overlays;};
+      overlays = [
+        purescript-overlay.overlays.default
+        purescm.overlays.purescript
+      ];
+      pkgs = import nixpkgs { inherit system overlays; };
       vite = pkgs.writeShellApplication {
         name = "vite";
         runtimeInputs = with pkgs; [ nodejs_20 ];
         text = "npx vite --open";
       };
+      chez = pkgs.chez.overrideAttrs (final: prev: {
+        postFixup = if pkgs.stdenv.isDarwin then ''
+          install_name_tool -add_rpath ${pkgs.pcre2.out}/lib $out/bin/scheme
+        '' else ''
+          patchelf $out/bin/scheme --add-rpath ${pkgs.pcre2.out}/lib
+        '';
+      });
+      # pursBackendScm = pkgs.purs-backend-scm;
     in
       pkgs.mkShell {
         inherit name;
@@ -52,7 +57,9 @@
             pkgs.spago-unstable
             pkgs.vite
 
-            # inputs.purescm
+            # Chez Scheme compiler and PureScript backend
+            chez
+            # pursBackendScm
           ]
           ++ (pkgs.lib.optionals (system == "aarch64-darwin")
             (with pkgs.darwin.apple_sdk.frameworks; [
